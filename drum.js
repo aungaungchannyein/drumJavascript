a4e88293606fd4a459197190d840e9c93a7bad95
ChanNyein AungAung <44551834+aungaungchannyein@users.noreply.github.com>
2022-02-17 15:53:32 +0630

    Add files via upload

---
+++ drum.js
@@ -0,0 +1,79 @@
+list=document.querySelectorAll("button")
+
+for (var i=0; i<list.length; i++){
+
+	document.querySelectorAll("button")[i].addEventListener("click", function(){
+	
+	var buttonInnerHTML =this.innerHTML;
+
+	makeSound(buttonInnerHTML);
+
+	buttonAnimation(buttonInnerHTML)
+});
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key){
+
+	switch(key){
+		case "w":
+			var tom1 = new Audio("sounds/tom-1.mp3");
+			tom1.play();
+			break;
+
+		case "a":
+			var tom1 = new Audio("sounds/tom-2.mp3");
+			tom1.play();
+			break;
+
+		case "s":
+			var tom1 = new Audio("sounds/tom-3.mp3");
+			tom1.play();
+			break;
+
+		case "d":
+			var tom1 = new Audio("sounds/tom-4.mp3");
+			tom1.play();
+			break;
+
+		case "j":
+			var tom1 = new Audio("sounds/snare.mp3");
+			tom1.play();
+			break;
+
+		case "k":
+			var tom1 = new Audio("sounds/crash.mp3");
+			tom1.play();
+			break;
+
+		case "l":
+			var tom1 = new Audio("sounds/kick-bass.mp3");
+			tom1.play();
+			break;
+
+
+	}
+
+	
+}
+
+function buttonAnimation(currentKey){
+	var activeButton = document.querySelector("."+currentKey);
+
+	activeButton.classList.add("pressed");
+
+	setTimeout(function(){
+		activeButton.classList.remove("pressed")
+	}, 100)
+}
+
+
